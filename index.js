6efedc5ab93d216cad8f4251b7bf38292c68da52
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-06 03:40:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(array) {
-    return [array[0], array[1]];
-  }
+  return [array[0], array[1]];
+}
+function returnLastTwoDrivers(array) {
+  return [array[array.length-1], array[array.length-2]],
+}
